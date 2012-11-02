455b2a70a030efd896a6a7b7bf321c9ebd1f1fc3
Rohit Agarwal <rohita@apigee.com>
2012-11-02 10:45:46 +0530

    samples from Rohit

---
+++ setenv.sh
@@ -0,0 +1,15 @@
+##Update below section for your environment
+## --------------------------------------
+
+org=<ORG>
+environment=<ENV>
+credentials=<USER>:<PASSWD>
+url=<SERVER_URL>
+
+## DONNOT change anything below this line
+## --------------------------------------
+export org=$org
+export environment=$environment
+export credentials=$credentials
+export url=$url
+
