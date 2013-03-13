7760627473bcdbe12fd93520f61bb3c2665025f9
Andrew Brown <ambrown@gmail.com>
2013-03-13 11:54:12 -0400

    configured a more minimal policy

--- sample-proxies/xmltojson/invoke_pretty_print.sh
+++ sample-proxies/xmltojson/invoke_pretty_print.sh
@@ -4,6 +4,6 @@ echo Using org and environment configured in /setup/setenv.sh, $org and $env
 
 source ../../setup/setenv.sh
 
-# curl http://$org-$env.apigee.net/xmltojson/forecastrss?"w=12797282" | python -mjson.tool
+curl http://$org-$env.apigee.net/xmltojson/forecastrss?"w=12797282" | python -mjson.tool
 
-curl http://$org-$env.apigee.net/xmltojson/forecastrss?"w=12797282" | xmllint --format
+# curl http://$org-$env.apigee.net/xmltojson/forecastrss?"w=12797282" | xmllint --format
