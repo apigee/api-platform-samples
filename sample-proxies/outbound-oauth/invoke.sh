eb61c0d5fe1ff110269066e533d353ebf00f48d6
Andrew Brown <ambrown@gmail.com>
2013-11-27 11:30:33 -0500

    Updated to reflect new path of proxy

--- sample-proxies/outbound-oauth/invoke.sh
+++ sample-proxies/outbound-oauth/invoke.sh
@@ -4,6 +4,8 @@ echo Using org and environment configured in /setup/setenv.sh
 
 source ../../setup/setenv.sh
 
+echo "Make sure you set your Azure credentials in /resources/jsc/api-config.js"
+
 set -x
 
-curl "http://$org-$env.apigee.net/outbound-oauth"
+curl "http://$org-$env.apigee.net/outbound-oauth/search.json?lang=es&q=bicycle"
