d78ba7adf5243f5bea71e8e6bf90674d04be60d0
wwitman <wwitman@apigee.com>
2014-02-11 16:42:49 -0800

    Removed my info

--- doc-samples/nodejs-baas-cookbook/apiproxy/resources/node/config.js
+++ doc-samples/nodejs-baas-cookbook/apiproxy/resources/node/config.js
@@ -2,11 +2,9 @@
  * Edit the defaults for your environment
  */
 
-exports.organization = 'wwitman'
-exports.application = 'employees'
-exports.clientId = 'b3U649QSGirPEeOaxushYqsjXw'
-exports.clientSecret = 'b3U6uDu_f8_I6kfGMrWZLftVbiaTHmU'
-exports.username = 'jdoe'
-exports.password = 'Welcome1'
+exports.organization = 'Enter your organization name here.'
+exports.application = 'Enter your application name here'
+exports.clientId = 'Enter the Client ID here.'
+exports.clientSecret = 'Enter the Client Secret ID here.'
 exports.tokenExpiration = 60000
 exports.logging = true
