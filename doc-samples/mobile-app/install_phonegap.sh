09467d9483042e93bfe74b96e5bf7198acbde047
Andrew Brown <ambrown@gmail.com>
2014-02-24 14:21:46 -0500

    Replaced wget with -O option for one less dependency.

--- doc-samples/mobile-app/install_phonegap.sh
+++ doc-samples/mobile-app/install_phonegap.sh
@@ -1,3 +1,6 @@
 # Be sure you have npm installed and running
 
+echo Unfortunately, the PhoneGap install requires sudo
+echo The requested password is your system password (specific to your machine)
+
 sudo npm install phonegap
