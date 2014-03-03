b20976ffa7e260e0c1e5aeb22d8411413b078aa8
Andrew Brown <ambrown@gmail.com>
2014-03-03 11:11:03 -0500

    Listen only on HTTPS, since credentials are being passed.

--- sample-proxies/base64encoder/invoke.sh
+++ sample-proxies/base64encoder/invoke.sh
@@ -6,4 +6,4 @@ source ../../setup/setenv.sh
 
 set -x
 
-curl "http://$org-$env.apigee.net/base64encoder?username=MyUserName&password=MyPassword" -v
+curl "https://$org-$env.apigee.net/base64encoder?username=MyUserName&password=MyPassword" -v
