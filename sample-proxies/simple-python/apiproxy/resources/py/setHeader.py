6877f9e076beaf786d444062b65ec722770a65fa
wwitman <wwitman@apigee.com>
2015-09-27 21:36:17 -0600

    changed variable

--- sample-proxies/simple-python/apiproxy/resources/py/setHeader.py
+++ sample-proxies/simple-python/apiproxy/resources/py/setHeader.py
@@ -1,2 +1,2 @@
-response.setVariable("header.X-Apigee-Demo-target", flow.getVariable("target.name"));
+response.setVariable("header.X-Apigee-Demo-target", flow.getVariable("target.url"));
 print 'Reached the script & assigned header variable' 
