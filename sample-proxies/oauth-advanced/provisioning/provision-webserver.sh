1f3e9bcff343a205fb834058b4cfef2ad3c2190a
dzuluaga <dzuluaga@apigee.com>
2015-10-09 20:25:03 -0700

    Revert "Revert "rename references to oauth2-app to webserver-app. add login-app to oauth2-product for app name visibility after logging in. cleanup README""
    This reverts commit 425b66f5a591542de86bd6a9b222e00b906a7dba.

--- sample-proxies/oauth-advanced/provisioning/provision-webserver.sh
+++ sample-proxies/oauth-advanced/provisioning/provision-webserver.sh
@@ -11,18 +11,18 @@ echo using $username and $org
 echo Install API Products
 
 curl -u $username:$password $url/v1/o/$org/apiproducts \
-  -H "Content-Type: application/json" -X POST -T oauth2-product.json
+  -H "Content-Type: application/json" -X POST -T webserver-product.json
 
 echo Create developers
 
 curl -u $username:$password $url/v1/o/$org/developers \
-  -H "Content-Type: application/xml" -X POST -T oauth2-developer.xml
+  -H "Content-Type: application/xml" -X POST -T webserver-developer.xml
 
 echo Create apps
 
 curl -u $username:$password \
-  $url/v1/o/$org/developers/oauth2dev@example.com/apps \
-  -H "Content-Type: application/xml" -X POST -T oauth2-app.xml
+  $url/v1/o/$org/developers/webdev@example.com/apps \
+  -H "Content-Type: application/xml" -X POST -T webserver-app.xml
 
 
 # Get consumer key and attach API product
@@ -30,19 +30,19 @@ curl -u $username:$password \
 
 echo Get app info
 curl -u $username:$password -H "Accept: application/json" \
-     $url/v1/o/$org/developers/oauth2dev@example.com/apps/oauth2-app
+     $url/v1/o/$org/developers/webdev@example.com/apps/webserver-app
 
 key=`curl -u $username:$password -H "Accept: application/json" \
-     $url/v1/o/$org/developers/oauth2dev@example.com/apps/oauth2-app 2>/dev/null \
+     $url/v1/o/$org/developers/webdev@example.com/apps/webserver-app 2>/dev/null \
      | grep consumerKey | awk -F '\"' '{ print $4 }'`
 
 secret=`curl -u $username:$password -H "Accept: application/json" \
-     $url/v1/o/$org/developers/oauth2dev@example.com/apps/oauth2-app 2>/dev/null \
+     $url/v1/o/$org/developers/webdev@example.com/apps/webserver-app 2>/dev/null \
      | grep consumerSecret | awk -F '\"' '{ print $4 }'`
 
 curl -u $username:$password \
-  $url/v1/o/$org/developers/oauth2dev@example.com/apps/oauth2-app/keys/${key} \
-  -H "Content-Type: application/xml" -X POST -T oauth2-product.xml
+  $url/v1/o/$org/developers/webdev@example.com/apps/webserver-app/keys/${key} \
+  -H "Content-Type: application/xml" -X POST -T webserver-product.xml
 
-echo "\n\nConsumer key for oauth2-app is ${key}"
-echo "\n\nConsumer secret for oauth2-app is ${secret}"
+echo "\n\nConsumer key for webserver-app is ${key}"
+echo "\n\nConsumer secret for webserver-app is ${secret}"
