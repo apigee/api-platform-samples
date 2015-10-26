cb897f073a76c814c803d4972a929182de51f4ec
wwitman <wwitman@apigee.com>
2015-10-26 10:46:01 -0600

    added echoes

--- doc-samples/nodejs-baas-cookbook/invoke.sh
+++ doc-samples/nodejs-baas-cookbook/invoke.sh
@@ -6,10 +6,12 @@ source ../../setup/setenv.sh
 
 set -x
 
-
+echo -e "\nPOST employee data to the back-end data store.\n"
 curl http://$org-$env.apigee.net/employees/profile \
 -H "Content-Type: application/json" \
 -d '{"id":"ajones", "firstName":"Alice", "lastName":"Jones", "phone": "201-555-5555" }' \
 -X POST
 
+
+echo -e "\nFetch employee data from the back-end data store.\n"
 curl "http://$org-$env.$api_domain/employees/profiles"
