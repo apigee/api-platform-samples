cf94be043a6a921b825e36f0a2c5167b646c4e11
wwitman <wwitman@apigee.com>
2015-12-02 16:05:12 -0700

    fixed path

--- samplets/Extract-Variables/invoke.sh
+++ samplets/Extract-Variables/invoke.sh
@@ -2,12 +2,12 @@
 
 source ../../setup/setenv.sh
 
-echo -e "\n\nAPI 1 of 2: http://$org-$env.$api_domain/parse-request-url/extract-variables/resource1/123456"
+echo -e "\n\nAPI 1 of 2: http://$org-$env.$api_domain/extract-variables/resource1/123456"
 read -p "Press Return to call this API..."
-curl "http://$org-$env.$api_domain/parse-request-url/extract-variables/resource1/123456" 
-echo -e "\n\nAPI 2 of 2: http://$org-$env.$api_domain/parse-request-url/extract-variables?code=abc123"
+curl "http://$org-$env.$api_domain/extract-variables/resource1/123456" 
+echo -e "\n\nAPI 2 of 2: http://$org-$env.$api_domain/extract-variables?code=abc123"
 read -p "Press Return to call this API..."
-curl "http://$org-$env.$api_domain/parse-request-url/extract-variables?code=DBNabc123" 
+curl "http://$org-$env.$api_domain/extract-variables?code=DBNabc123" 
 echo -e "\n\n"
 
 
