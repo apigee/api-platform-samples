7c7636357cc0c406f892deae7a2259cc291ab65b
wwitman <wwitman@apigee.com>
2015-12-02 15:19:11 -0700

    renaming

--- samplets/Parse-Request-URL/invoke.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ../../setup/setenv.sh
-
-echo -e "Calling http://$org-$env.$api_domain/parse-request-url/extract-variablels/resource1/123456"
-
-
-curl "http://$org-$env.$api_domain/parse-request-url/extract-variables/resource1/123456" 
-
