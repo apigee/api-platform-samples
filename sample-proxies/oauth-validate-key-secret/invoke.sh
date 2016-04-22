6135969e3c6697acccafdbb21c9231d556ef590a
wwitman <wwitman@apigee.com>
2016-04-22 10:41:05 -0600

    standardized the invoke script

--- sample-proxies/oauth-validate-key-secret/invoke.sh
+++ sample-proxies/oauth-validate-key-secret/invoke.sh
@@ -1,12 +1,66 @@
 #!/bin/bash
 
-echo Using org and environment configured in /setup/setenv.sh, $org and $env
+printf "\nDid you enter your Edge configuration information in ../../setup/setenv.sh? [y/n]: "
+read setenv
 
-echo "Enter a Base64 encoded client ID and client secret for a developer app associated with this proxy:"
-read basic
+if [ -z $setenv ] || [ "$setenv" = "y" ]; then
+  printf ""
+else  
+  printf "\nYou must configure this file before continuing. See the README for details. Press Return to exit."
+  read
+  exit
+fi
+
+
+printf "\nDid you run the provisioning script ../../setup/provisioning/setup.sh? [y/n]: "
+read setup
+
+if [ -z $setup ] || [ "$setup" = "y" ]; then
+  printf ""
+else  
+  printf "\nYou must run this script before continuing. See the README for details. Press Return to exit."
+  read
+  exit
+fi
 
 source ../../setup/setenv.sh
-echo "Basic: " $basic
+
+printf "\nEnter your password for the Apigee Enterprise organization $org, followed by [ENTER]: " 
+
+read -s password
+
+
+
+printf  "\n\nFetching consumer key and secret for Josiah's Weather App from Apigee Edge..."
+ks=`curl -u "$username:$password" "$url/v1/o/$org/developers/joe@weathersample.com/apps/joe-app" 2>/dev/null | egrep "consumer(Key|Secret)"`
+key=`echo $ks | awk -F '\"' '{ print $4 }'`
+secret=`echo $ks | awk -F '\"' '{ print $8 }'`
+
+
+printf "\nRetrieved key: $key"
+printf "\nRetrieved secret: $secret\n"
+
+printf "\n\nBase64 encrypt the key:secret values for the Basic Auth header: "
+
+auth=`echo -n $key:$secret | base64`
+
+printf "\nBasic: $auth"
+
+printf  "\n\nRequest the access token using the retrieved keys... \n"
  
-curl https://$org-$env.$api_domain/oauth-validate-key-secret/token -H "Authorization: Basic $basic" -d 'grant_type=password&username=apigeeuser&password=Apigee123' -H 'Content-Type: application/x-www-form-urlencoded'
+#curl https://$org-$env.$api_domain/oauth-validate-key-secret/token -H "Authorization: Basic $auth" -d 'grant_type=password&username=apigeeuser&password=Apigee123' -H 'Content-Type: application/x-www-form-urlencoded'
+
+
+
+
+
+
+
+
+accesstoken_response=`curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H "Authorization: Basic $auth" "https://$org-$env.$api_domain/oauth-validate-key-secret/token" -d 'grant_type=password&username=apigeeuser&password=Apigee123'`
+
+printf  "curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -H \"Authorization: Basic $auth\"           \"https://$org-$env.$api_domain/oauth-validate-key-secret/token -d grant_type=password&username=apigeeuser&password=Apigee123\" \n"
+
+printf  "\nAccessToken Response: \n\n $accesstoken_response \n"
+
 
