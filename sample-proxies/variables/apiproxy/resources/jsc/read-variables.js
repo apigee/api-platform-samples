2d2dde827f7363b27a8a7a8e98bc44d9bc55714c
wwitman <wwitman@apigee.com>
2016-05-12 09:45:36 -0600

    refactored this sample to use mocktarget

---
+++ sample-proxies/variables/apiproxy/resources/jsc/read-variables.js
@@ -0,0 +1,11 @@
+ var a = context.getVariable("mock.city");
+print(a);
+
+var b = context.getVariable("mock.state");
+print(b);
+
+var c = context.getVariable("mock.firstName");
+print(c);
+
+var d = context.getVariable("mock.lastName");
+print(d);
