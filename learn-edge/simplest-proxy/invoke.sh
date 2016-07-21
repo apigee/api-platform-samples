767ebd994d746999e9271f8aadf2e19bbf1623d6
wwitman <wwitman@apigee.com>
2016-07-21 14:57:44 -0600

    deleted unneeded step

--- learn-edge/simplest-proxy/invoke.sh
+++ learn-edge/simplest-proxy/invoke.sh
@@ -4,15 +4,9 @@
 
 source ../scripts/set_env.sh
 
-printf "\nEnter your password for the Apigee Enterprise organization $org, followed by [ENTER]:\n"
-read -s password
-
-source ../scripts/verify_credentials.sh
-
-
 ## Call the API
 
-printf "Now, we will call the API proxy with this curl command: curl -i http://$org-$env.$api_domain/v1/learn-edge\n\n"
+printf "\n\nNow, we will call the API proxy with this curl command: curl -i http://$org-$env.$api_domain/v1/learn-edge\n\n"
 printf "Hint: You will see a 200 HTTP status if the call succeeds.\n\n"
 printf "Press return to execute the call: \n"
 read
