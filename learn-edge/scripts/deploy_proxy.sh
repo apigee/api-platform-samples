3fe233c9e58752dbb475c86d0aab5a86c470b5c8
Floyd Jones <fjones@apigee.com>
2016-07-25 11:36:16 -0600

    Update script with directory option

--- learn-edge/scripts/deploy_proxy.sh
+++ learn-edge/scripts/deploy_proxy.sh
@@ -3,7 +3,7 @@
 function deploy_proxy {
     if hash apigeetool 2>/dev/null; then
     	printf "\n\nUsing apigeetool to deploy the proxy to the $env environment in the $org org...\n\n"
-        apigeetool deployproxy -o $org -e $env -n learn-edge -L $url -u $username -p $password -V
+        apigeetool deployproxy -o $org -e $env -n learn-edge -L $url -u $username -d . -p $password -V
         printf "\nIf 'State: deployed', then your API Proxy is ready to be invoked.\n"
         printf "\nRun 'invoke.sh'\n"
     else
