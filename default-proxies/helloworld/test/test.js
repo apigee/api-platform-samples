2a3104ab3d663c3b7bcff36223425bc1c2711dfd
Floyd Jones <fjones@apigee.com>
2016-09-07 16:47:41 -0600

    internal seed update

--- default-proxies/helloworld/test/test.js
+++ default-proxies/helloworld/test/test.js
@@ -1,40 +1,19 @@
-var assert = chai.assert;
+var expect  = require("chai").expect;
+var request = require("request");
+// Configure and uncomment the following for local testing:
+// var org = "your_org"; 
+// var env = "test";
 
-var pathname = window.location.pathname
-var splits = pathname.split('/')
-//expected /v1/o/:orgname/e/:env/samples/:sample/test.html
-var org = splits[3]
-var env = splits[5]
-var sample = splits[7]
 
-getTestData(org,env,sample,function(data){
-	//No data for this proxy
-})
-
-var url = 'https://' + org + '-' + env + '.apigee.net/v0/hello'
-
-describe('Running test for helloworld', function(){
-    describe('calling ' + url, function(){
-        it('call the proxy', function(done) {
-
-          $.ajax({
-              url:url,
-              method: 'GET',
-              complete:function(xhr,statusText){ next(null,xhr.status),
-              success:function(body){ 
-                console.log(body)
-                assert(body)
-                done()
-              },
-              function(cberror,codes){
-				var success_200 = 0
-				codes.forEach(function(s){ if (s==200) success_200++})
-				assert.equal(2,success_200)
-				done(cberror),
-              error: function(xhr,status,error){
-                done(error)
-              }
-          }
-      })
-   })
-})
+var url = "https://" + org + "-" + env + ".apigee.net/v0/hello"
+//var url = "https://apigeedocs-test.apigee.net/v0/hello"
+describe("Running test for helloworld", function(){
+    describe("Doing a GET on " + url, function(){
+        it("returns status 200", function(done) {
+            request(url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+         });
+      });
+   });
+});
