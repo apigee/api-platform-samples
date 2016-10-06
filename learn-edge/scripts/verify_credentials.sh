d8e4c5410f46d3a947763d24f7c8bcfa916b5819
stephen gilson <sgilson@apigee.com>
2016-10-06 13:06:30 -0400

    add zip file for apikey-security-1 and use url param in verify_credentials.sh instead of hard-coded url.

--- learn-edge/scripts/verify_credentials.sh
+++ learn-edge/scripts/verify_credentials.sh
@@ -1,6 +1,6 @@
 printf "\n\nVerifying credentials..."
 
-response=`curl -s -o /dev/null -I -w "%{http_code}" https://api.enterprise.apigee.com/v1/organizations/$org -u $username:$password`
+response=`curl -s -o /dev/null -I -w "%{http_code}" $url/v1/organizations/$org -u $username:$password`
 
 if [ $response -eq 401 ]
 then
