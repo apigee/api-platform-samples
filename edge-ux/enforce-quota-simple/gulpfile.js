495c7e010dbfc407c5a3187954d3e2fe209c262a
Steve Traut <straut@apigee.com>
2016-11-20 11:30:15 -0500

    Adding quota sample.

---
+++ edge-ux/enforce-quota-simple/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var apigeetool = require('apigeetool')
+var gutil = require('gulp-util')
+var proxy_name = 'enforce-quota-simple'
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+var opts = {
+    organization: gutil.env.org,
+    token: gutil.env.token,
+    environments: gutil.env.env,    
+    environment: gutil.env.env,
+    debug: gutil.env.debug 
+}
+
+gulp.task('deploy',function(){
+	opts.api = proxy_name
+	return apigeetool.getPromiseSDK()
+		.deployProxy(opts)
+})
