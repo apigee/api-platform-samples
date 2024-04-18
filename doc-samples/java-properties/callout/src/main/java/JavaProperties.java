3c2df4d5c35917462532c9bcee9377520297f8f4
Dino Chiesa <dchiesa@google.com>
2024-04-18 10:36:14 -0700

    fix: big update for java callouts
    - update buildsetup.sh scripts to point to the
      new repo, which is in Artifact Repository
    
    - the script also installs the JARs into different groupid's, in
      alignment with the documentation page "how to create a Java
      callout".
    
    - modify the deploy.sh script to emit a message saying that it is
      intended to work only with Apigee Edge.
    
    - update all the pom.xml files to refer to those new groupid's, and the
      new repository, and also to remove extraneous stuff. and get current
      on plugin versions, and Java SE version (1.8).
    
    - add the appropriate copyright notice to the script, and to
      the Java sources.
    
    - modify the policy names to conform to Apigeelint recommendations.
    
    - reformat the Java source code with google-java-format
    
    - remove the http callout in the java-cookbook - this is now prohibited
      by Apigee permissions restrictions! (though it can be relaxed in hybrid)

--- doc-samples/java-properties/callout/src/main/java/JavaProperties.java
+++ doc-samples/java-properties/callout/src/main/java/JavaProperties.java
@@ -1,30 +1,44 @@
+// Copyright © 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 package com.apigeesample;
 
 import com.apigee.flow.execution.ExecutionContext;
 import com.apigee.flow.execution.ExecutionResult;
 import com.apigee.flow.execution.spi.Execution;
 import com.apigee.flow.message.MessageContext;
-
 import java.util.Map;
 
 public class JavaProperties implements Execution {
 
-	private Map <String,String> properties; // read-only
+  private Map<String, String> properties; // read-only
 
-	public JavaProperties(Map <String,String> properties) {
-	        this.properties = properties;
-	}
+  public JavaProperties(Map<String, String> properties) {
+    this.properties = properties;
+  }
 
-	public ExecutionResult execute(MessageContext messageContext, ExecutionContext executionContext) {
-		
-		try {
+  public ExecutionResult execute(MessageContext messageContext, ExecutionContext executionContext) {
 
-		    messageContext.getMessage().setHeader("X-PROPERTY-HELLO", this.properties.get("prop"));
-            
-            return ExecutionResult.SUCCESS;
+    try {
 
-		} catch (Exception e) {
-			return ExecutionResult.ABORT;
-		}
-	}
+      messageContext.getMessage().setHeader("PROPERTY-Prop1", this.properties.get("prop1"));
+
+      return ExecutionResult.SUCCESS;
+
+    } catch (Exception e) {
+      return ExecutionResult.ABORT;
+    }
+  }
 }
